39f13e8a01a8047180783bdfab1c472e146a940e
Ashu <yash6102000thakur@gmail.com>
2022-03-12 22:22:36 +0530

    final

--- src/component/js/Project.js
+++ src/component/js/Project.js
@@ -11,28 +11,33 @@ export default function Project({ mode }) {
         </p>
       </div>
      <div id="one" className="projectOne">
-     <h2    className="projectOne_h2"> 1 | 6</h2>
-       <h1    className="projectOne_h1">Spotify-Clone</h1>
-       <p style={{ color: mode === "white" ? "black" : "white" }} > A spofity-clone where user and experince the music with good UI interfarence . get the source code from github</p>
+     <h2    className="projectOne_h2"> 1 | 4</h2>
+       <h1    className="projectOne_h1">Disney-Clone</h1>
+       <p style={{ color: mode === "white" ? "black" : "white" }} >Stacking - NEXT.JS | REACT.JS | NEXTAUTH | TAILWIND CSS | FIREBASE</p>
      </div>
-     <Link className="links"  to="/">details</Link>
+     <a  target="_blank" rel="noreferrer" className="links"  href="https://github.com/Ashu06102000/disneyPlusClone">details</a>
 
      <div className="projectOne">
-     <h2    className="projectOne_h2"> 2 | 5</h2>
+     <h2    className="projectOne_h2"> 2 | 4 </h2>
        <h1    className="projectOne_h1">News Web Application</h1>
-       <p style={{ color: mode === "white" ? "black" : "white" }} > A simple news web-app which render news on real-time of each caterogy like General , health , science ... etc. by using  News Api which is  a open source API for developer </p>
+       <p style={{ color: mode === "white" ? "black" : "white" }} >Stacking - REACT.JS | HTML | CSS | NEWS-API</p>
      </div>
      <a  target="_blank" rel="noreferrer" className="links"  href="https://github.com/Ashu06102000/NewsWeb">details</a>
     
-     <a  target="_blank" rel="noreferrer" className="links"  href="https://github.com/Ashu06102000/cryptoency">details</a>
+   
      <div className="projectOne">
-     <h2    className="projectOne_h2"> 3 | 5</h2>
+     <h2    className="projectOne_h2"> 3 | 4</h2>
        <h1   className="projectOne_h1">Netflix-Clone</h1>
-       <p style={{ color: mode === "white" ? "black" : "white" }} > Netflix-clone which fetch the movies from TMDB APi database which is provide details of movies and shows. and also free for development use .</p>
+       <p style={{ color: mode === "white" ? "black" : "white" }} > Stacking - REACT.JS | HTML | CSS | TMDB-API</p>
      </div>
-     <a target="_blank" rel="noreferrer" className="links"  href="https://github.com/Ashu06102000/NetflixClone">details</a>
+     <a target="_blank" rel="noreferrer" className="links"  href="https://github.com/Ashu06102000/NetflixClone">details</a>   <div className="projectOne">
+     <h2    className="projectOne_h2"> 4 | 4</h2>
+       <h1   className="projectOne_h1">Hidden-leaf restaurant</h1>
+       <p style={{ color: mode === "white" ? "black" : "white" }} > Stacking - NEXT.JS | REACT.JS | REDUX | MONGODB | TAILWIND CSS</p>
+     </div>
+     <a target="_blank" rel="noreferrer" className="links"  href="https://github.com/Ashu06102000/Hidden-Leaf-restaurant">details</a>
     
-     <a target="_blank" rel="noreferrer" className="links"  href="https://github.com/Ashu06102000/x-box-website">details</a>
+     
       <button onClick={() => console.log("hire me")}> <Link style={{color:'black' , textDecoration:'none'}}  to="/contact">WORK WITH ME</Link> </button>
     </>
   );
